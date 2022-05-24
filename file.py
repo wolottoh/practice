7ee05e409a094c064e442394a2c54e8c2582bfe6
wolottoh <105494563+wolottoh@users.noreply.github.com>
2022-05-24 15:55:37 +0100

    Add files via upload

---
+++ file.py
@@ -0,0 +1,9 @@
+user_funds = 10.31
+item_price = price["Burger"]
+
+if item_price < user_funds:
+    Print("You have enough money!")
+if item_price = user_funds:
+    print("You have the precise amount of money")
+if item_price < user_funds:
+    print('Sorry you don't have enough money')
